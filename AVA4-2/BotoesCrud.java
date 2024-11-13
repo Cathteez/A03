b98007c7caeeedb32a0596be08570e3611f4fb47
mcbarzotto <maria.nbarzotto@sempreceub.com>
2024-11-13 12:35:40 -0300

    Add files via upload

---
+++ AVA4-2/BotoesCrud.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class BotoesCRUD {
+    public static JPanel criarPainelBotoes(JDialog dialog) {
+        JPanel painelBotoes = new JPanel(new FlowLayout());
+        String[] labels = {"Incluir", "Alterar", "Excluir", "Consultar", "Cancelar", "Sair"};
+        for (String label : labels) {
+            JButton button = new JButton(label);
+            if (label.equals("Sair")) {
+                button.addActionListener(e -> dialog.setVisible(false));
+            } else {
+                button.addActionListener(e -> JOptionPane.showMessageDialog(dialog, "Botão " + label + " clicado!"));
+            }
+            painelBotoes.add(button);
+        }
+        return painelBotoes;
+    }
+}
---
+++ AVA4-2/SistemaPessoa.java
@@ -0,0 +1,10 @@
+
+/*
+    Classe de inicializa a aplicação.
+ */
+public class SistemaPessoa {
+    public static void main(String[] args) {
+        // Inicia a aplicação com o menu principal
+        new MenuPrincipal();
+    }
+}
